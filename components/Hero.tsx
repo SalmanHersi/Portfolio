1b39e3e3a93cf8ac718676bd7dec63921f4f6eca
SalmanHersi <hersi.salman@gmail.com>
2023-10-13 13:11:05 -0400

    fix build error eslint

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,9 +1,17 @@
 import React from 'react';
+import Image from 'next/image';
 
 const Hero = () => {
   return (
-    <div className='container mx-auto flex w-full max-w-7xl flex-col p-5'>
-      <h3>Hello 👋, I'm Salman Hersi</h3>
+    <div className='container mx-auto mt-7 flex w-full max-w-7xl flex-col p-5 text-center'>
+      <h1 className=' text-3xl'>Hello 👋, I'm Salman Hersi</h1>
+      <Image
+        className=' rounded-lg'
+        src={'/imgs/Headshot-Clear 2.jpg'}
+        width={300}
+        height={300}
+        alt='Avatar'
+      ></Image>
       <p>
         I'm a Full Stack Developer, specializing in crafting innovative digital
         solutions with a keen focus on intuitive and dynamic user experiences.
