f8192fd6752297c1a8e02732cae9e7e692f0a180
SalmanHersi <hersi.salman@gmail.com>
2023-10-13 15:20:19 -0400

    add profile picture and personal blurb

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -3,16 +3,19 @@ import Image from 'next/image';
 
 const Hero = () => {
   return (
-    <div className='container mx-auto mt-7 flex w-full max-w-7xl flex-col p-5 text-center'>
+    <div className='container mx-auto mt-7 flex w-full max-w-7xl flex-col justify-center space-y-4 p-5 text-center'>
       <h1 className=' text-3xl'>Hello 👋, I'm Salman Hersi</h1>
-      <Image
-        className=' rounded-lg'
-        src={'/imgs/Headshot-Clear 2.jpg'}
-        width={300}
-        height={300}
-        alt='Avatar'
-      ></Image>
-      <p>
+      <div className='mx-auto'>
+        <Image
+          className='rounded-full shadow-lg'
+          src={'/imgs/Headshot-Clear 2.jpg'}
+          width={250}
+          height={200}
+          alt='Avatar'
+        ></Image>
+      </div>
+
+      <p className=' p-4'>
         I'm a Full Stack Developer, specializing in crafting innovative digital
         solutions with a keen focus on intuitive and dynamic user experiences.
       </p>
