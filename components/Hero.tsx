71e072994fe111b06098a98346b12795b3709ef4
SalmanHersi <hersi.salman@gmail.com>
2023-10-14 07:40:43 -0400

    fix padding and margin

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -7,8 +7,10 @@ import Link from 'next/link';
 const Hero = () => {
   return (
     <div className='container mx-auto  flex flex-col items-center md:flex-row '>
-      <div className='mx-auto mt-14 flex flex-col items-center justify-center space-y-8 p-6 text-center md:mt-36 md:items-start md:space-y-9 md:text-left md:text-2xl'>
-        <h1 className=' text-3xl md:text-4xl '>Hello 👋, I'm Salman Hersi</h1>
+      <div className='mx-auto mt-12 flex flex-col items-center justify-center space-y-6 p-3 text-center md:mt-40 md:items-start md:space-y-9 md:text-left md:text-2xl'>
+        <h1 className=' text-3xl md:text-4xl '>
+          Hello <span className='animate-pulse'>👋 </span>, I'm Salman Hersi
+        </h1>
         <div className=' md:hidden'>
           <Image
             className='rounded-full shadow-lg '
@@ -38,7 +40,7 @@ const Hero = () => {
       </div>
       <div className='mx-auto mt-24 hidden md:flex md:items-center md:justify-center'>
         <Image
-          className=' rounded-full'
+          className='rounded-full'
           src={'/imgs/Headshot-Clear 2.jpg'}
           width={350}
           height={300}
