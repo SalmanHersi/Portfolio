4ad5164acf3177b981cd7bdf3a95dc6c62886f02
SalmanHersi <hersi.salman@gmail.com>
2024-05-08 22:10:59 -0400

    added a new recent project

--- components/Showcase.tsx
+++ components/Showcase.tsx
@@ -1,3 +1,14 @@
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from '@/components/ui/card';
+
+import Image from 'next/image';
+
 export default function Showcase() {
   return (
     <div className='container flex flex-col items-center   md:flex-row'>
@@ -5,6 +16,28 @@ export default function Showcase() {
         <h1 className=' scroll-m-20 text-3xl font-semibold tracking-tight first:mt-0 '>
           Recent Projects
         </h1>
+        <Card>
+          <CardHeader>
+            <CardTitle className='w-[500px]'>GitSearch</CardTitle>
+            <CardDescription>
+              GitSearch is an advanced Github search application built with
+              React for component-based UIs, Next.js for server-side rendering
+              to enhance SEO and performance, and styled with TailwindCSS and
+              Chakra UI for a clean, efficient user experience.
+            </CardDescription>
+          </CardHeader>
+          <CardContent>
+            <Image
+              src='https://utfs.io/f/11dc0417-5243-4a21-8283-6761a2b32bb0-21uln.png'
+              width={500}
+              height={500}
+              alt='Picture of the author'
+            />
+          </CardContent>
+          <CardFooter>
+            <p>Card Footer</p>
+          </CardFooter>
+        </Card>
       </div>
     </div>
   );
