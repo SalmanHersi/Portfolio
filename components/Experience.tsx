95aba0a4c365eeb7adcc82666384cc20478a21c2
SalmanHersi <hersi.salman@gmail.com>
2024-05-14 13:05:15 -0400

    added more experience cards

--- components/Experience.tsx
+++ components/Experience.tsx
@@ -1,7 +1,35 @@
 export default function Experience() {
   return (
-    <div className='flex items-center justify-center md:p-6'>
-      <h3>Experience</h3>
+    <div className='flex flex-col items-center justify-center gap-6 p-5 '>
+      <div>
+        <h1 className=' scroll-m-20 pt-3 text-3xl font-semibold tracking-tight first:mt-0 md:mx-auto '>
+          Experience
+        </h1>
+      </div>
+      <div className=' max-w-5xl'>
+        <h3>E-Commerce Coordinator</h3>
+        <div>
+          <p>
+            Collaborated in a dynamic team to deliver comprehensive web design
+            solutions, including e-commerce platforms and cybersecurity, serving
+            250 clients across various industries, and conducted personalized
+            consultations to optimize their digital storefronts.
+          </p>
+        </div>
+      </div>
+      <div className='max-w-5xl'>
+        <h3>Software Developer</h3>
+        <div>
+          <p>
+            Worked with a development team to create responsive web applications
+            using best practices and SCRUM methodologies. Developed full-stack
+            applications and backend APIs to enhance front-end performance.
+            Communicated with clients to optimize web presence and user
+            experience, while building customized websites that improve site
+            traffic and sales management.
+          </p>
+        </div>
+      </div>
     </div>
   );
 }
