9e6e599a7361a9840f96cf99f5a2d8458019ea5c
SalmanHersi <hersi.salman@gmail.com>
2024-05-16 11:43:44 -0400

    added padding to various components

--- components/Showcase.tsx
+++ components/Showcase.tsx
@@ -15,14 +15,14 @@ import Image from 'next/image';
 export default function Showcase() {
   return (
     <div className='flex flex-col items-center justify-center   md:flex-row'>
-      <div className=' flex flex-col items-center justify-center space-y-7 p-4 text-center md:items-start md:space-y-10 md:pt-10 md:text-left md:text-2xl'>
+      <div className=' containerflex flex-col items-center justify-center space-y-7 p-4 text-center md:items-start md:space-y-10 md:pt-10 md:text-left md:text-2xl'>
         <h1 className=' scroll-m-20 text-3xl font-semibold tracking-tight first:mt-0 md:mx-auto  '>
           Recent Projects
         </h1>
-        <Card className=' mx-auto max-w-5xl transform duration-500 ease-in-out hover:scale-105 md:flex md:p-4'>
+        <Card className=' mx-auto max-w-5xl transform  duration-500 ease-in-out hover:scale-105 md:flex md:p-4'>
           <CardHeader className='flex gap-8'>
             <CardTitle className=''>GitSearch</CardTitle>
-            <CardDescription className='  max-w-md text-xl'>
+            <CardDescription className='   max-w-md text-xl'>
               GitSearch is an advanced Github search application built with
               React for component-based UIs, Next.js for server-side rendering
               to enhance SEO and performance, and styled with TailwindCSS and
